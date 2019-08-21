f87a923b1d58676a98d73e2fc07138f36946b0e2
956340199@qq.com <956340199@qq.com>
2019-08-21 17:07:25 +0800

    1.查看class文件构成的测试类

---
+++ src/main/java/com/yuan/JVMTest/TestClass.java
@@ -0,0 +1,15 @@
+package com.yuan.JVMTest;
+
+/**
+ * Author:袁航
+ * Date:2019-08-21 16:34
+ * Description:为了生成一个简单的class文件来查看class文件结构
+ */
+public class TestClass {
+
+    private int m;
+
+    public int inc(){
+        return m+1;
+    }
+}
